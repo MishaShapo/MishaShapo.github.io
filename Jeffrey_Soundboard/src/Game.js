a058cef7d38633c6fccbda9cc0e3599e9e49ac6e
Michail Shaposhnikov <misha.shapo98@gmail.com>
2015-11-11 22:56:44 -0600

    Added actual sounds

--- Jeffrey_Soundboard/src/Game.js
+++ Jeffrey_Soundboard/src/Game.js
@@ -1,4 +1,4 @@
-SoundIcon = function(index,imageKey,soundKey,game){
+SoundIcon = function(index,imageKey,soundKey,labelText,graphics,game){
   this.game = game;
   this.index = index;
   
@@ -12,9 +12,13 @@ SoundIcon = function(index,imageKey,soundKey,game){
   }
   
   //var x = 20 + (((this.game.world.width - 4 * 20)/3)) * (index % 3);
-  var x = 20 + (20 + 153) * (index % 3);
-  var y = 240 + Math.floor(index / 3) * 136;
+ // var x = 20 + (20 + 240) * (index % 1);
+  var x = this.game.world.width / 2 - 140
+  var y = 240 + Math.floor(index / 1) * 245;
   this.button = this.game.add.button(x,y,imageKey,this.playSound,this);
+  this.label = this.game.add.text(x,y+170,labelText,{fill: "#ffffff"});
+  this.label.x = this.game.world.width/2 - this.label.width/2;
+  graphics.drawRoundedRect(x - 15, y - 15,280 + 30,222 + 10,15)
   
 }
 
@@ -27,6 +31,23 @@ JeffreySoundboard.Game = function (game) {
 JeffreySoundboard.Game.prototype = {
     create: function () {
       console.log('creating game');
+      var labels = [
+        "Aww Dang",
+        "Good Luck, Mang",
+        "It Was Really Noice",
+        "Lame",
+        "Hahahaha",
+        "Oh, Baby",
+        "Oh, Dang",
+        "RIP",
+        "Show Your Work",
+        "That's Pretty True Mang",
+        "They Cheated",
+        "Whoops"
+      ];
+      this.graphics = this.game.add.graphics();
+      this.graphics.lineStyle(5,Phaser.Color.hexToRGB("#fff"),1)
+      console.log(this.graphics);
       var title = this.game.add.image(this.game.world.centerX,100,"game_title");
       title.anchor.set(0.5);
       title.scale.set(1.6);
@@ -42,11 +63,11 @@ JeffreySoundboard.Game.prototype = {
         });
       this.game.kineticScrolling.start();
       var icons = [];
-      for(var i = 0; i < 7; i++){
+      for(var i = 0; i < 12; i++){
         var imageKey = "jeffrey_"+ (i+1);
         var soundKey = "jeffrey_" + (i+1) + "_sfx";
-        icons.push(new SoundIcon(i,imageKey,soundKey,this.game))
+        icons.push(new SoundIcon(i,imageKey,soundKey,labels[i],this.graphics,this.game))
       }
-       this.game.world.setBounds(0, 0, 3000, 3000);
+       this.game.world.setBounds(0, 0, 1000, 3500);
     }
 };
--- Jeffrey_Soundboard/src/Preloader.js
+++ Jeffrey_Soundboard/src/Preloader.js
@@ -18,20 +18,30 @@ JeffreySoundboard.Preloader.prototype = {
 		this.titleText = this.add.image(this.world.centerX, this.world.centerY- 200, 'titleImage');
 		this.titleText.anchor.setTo(0.5, 0.5);
         this.load.image('game_title',"asset/title.png");
-        this.load.image("jeffrey_1","asset/thmb/jeffrey_1.png");
-        this.load.image("jeffrey_2","asset/thmb/jeffrey_2.png");
-        this.load.image("jeffrey_3","asset/thmb/jeffrey_3.png");
-        this.load.image("jeffrey_4","asset/thmb/jeffrey_4.png");
-        this.load.image("jeffrey_5","asset/thmb/jeffrey_5.png");
-        this.load.image("jeffrey_6","asset/thmb/jeffrey_6.png");
-        this.load.image("jeffrey_7","asset/thmb/jeffrey_7.png");
-        this.load.audio('jeffrey_1_sfx', ['asset/sfx/jeffrey_1.mp3','asset/sfx/jeffrey_1.ogg','asset/sfx/jeffrey_1.m4a']);
-        this.load.audio('jeffrey_2_sfx', ['asset/sfx/jeffrey_2.mp3','asset/sfx/jeffrey_2.ogg','asset/sfx/jeffrey_2.m4a']);
-        this.load.audio('jeffrey_3_sfx', ['asset/sfx/jeffrey_3.mp3','asset/sfx/jeffrey_3.ogg','asset/sfx/jeffrey_3.m4a']);
-        this.load.audio('jeffrey_4_sfx', ['asset/sfx/jeffrey_4.mp3','asset/sfx/jeffrey_4.ogg','asset/sfx/jeffrey_4.m4a']);      
-        this.load.audio('jeffrey_5_sfx', ['asset/sfx/jeffrey_5.mp3','asset/sfx/jeffrey_5.ogg','asset/sfx/jeffrey_5.m4a']);
-        this.load.audio('jeffrey_6_sfx', ['asset/sfx/jeffrey_6.mp3','asset/sfx/jeffrey_6.ogg','asset/sfx/jeffrey_6.m4a']);
-        this.load.audio('jeffrey_7_sfx', ['asset/sfx/jeffrey_7.mp3','asset/sfx/jeffrey_7.ogg','asset/sfx/jeffrey_7.m4a']);
+        this.load.image("jeffrey_1","asset/thmb/jeffrey_01.png");
+        this.load.image("jeffrey_2","asset/thmb/jeffrey_02.png");
+        this.load.image("jeffrey_3","asset/thmb/jeffrey_03.png");
+        this.load.image("jeffrey_4","asset/thmb/jeffrey_04.png");
+        this.load.image("jeffrey_5","asset/thmb/jeffrey_05.png");
+        this.load.image("jeffrey_6","asset/thmb/jeffrey_06.png");
+        this.load.image("jeffrey_7","asset/thmb/jeffrey_07.png");
+        this.load.image("jeffrey_8","asset/thmb/jeffrey_08.png");
+        this.load.image("jeffrey_9","asset/thmb/jeffrey_09.png");
+        this.load.image("jeffrey_10","asset/thmb/jeffrey_10.png");
+        this.load.image("jeffrey_11","asset/thmb/jeffrey_11.png");
+        this.load.image("jeffrey_12","asset/thmb/jeffrey_12.png");
+        this.load.audio('jeffrey_1_sfx', ['asset/sfx/new/aww_dang.mp3','asset/sfx/new/aww_dang.ogg','asset/sfx/new/aww_dang.m4a']);
+        this.load.audio('jeffrey_2_sfx', ['asset/sfx/new/best_of_luck.mp3','asset/sfx/new/best_of_luck.ogg','asset/sfx/new/best_of_luck.m4a']);
+        this.load.audio('jeffrey_3_sfx', ['asset/sfx/new/it_was_really_noice.mp3','asset/sfx/new/it_was_really_noice.ogg','asset/sfx/new/it_was_really_noice.m4a']);
+        this.load.audio('jeffrey_4_sfx', ['asset/sfx/new/lame.mp3','asset/sfx/new/lame.ogg','asset/sfx/new/lame.m4a']);      
+        this.load.audio('jeffrey_5_sfx', ['asset/sfx/new/laugh.mp3','asset/sfx/new/laugh.ogg','asset/sfx/new/laugh.m4a']);
+        this.load.audio('jeffrey_6_sfx', ['asset/sfx/new/low_oh_baby.mp3','asset/sfx/new/low_oh_baby.ogg','asset/sfx/new/low_oh_baby.m4a']);
+        this.load.audio('jeffrey_7_sfx', ['asset/sfx/new/oh_dang.mp3','asset/sfx/new/oh_dang.ogg','asset/sfx/new/oh_dang.m4a']);
+        this.load.audio('jeffrey_8_sfx', ['asset/sfx/new/rip.mp3','asset/sfx/new/rip.ogg','asset/sfx/new/rip.m4a']);
+        this.load.audio('jeffrey_9_sfx', ['asset/sfx/new/show_your_work.mp3','asset/sfx/new/show_your_work.ogg','asset/sfx/new/show_your_work.m4a']);
+        this.load.audio('jeffrey_10_sfx', ['asset/sfx/new/thats_pretty_true_mang.mp3','asset/sfx/new/thats_pretty_true_mang.ogg','asset/sfx/new/thats_pretty_true_mang.m4a']);
+        this.load.audio('jeffrey_11_sfx', ['asset/sfx/new/they_cheated.mp3','asset/sfx/new/they_cheated.ogg','asset/sfx/new/they_cheated.m4a']);
+        this.load.audio('jeffrey_12_sfx', ['asset/sfx/new/whoops.mp3','asset/sfx/new/whoops.ogg','asset/sfx/new/whoops.m4a']);
         console.log("even finished preloading!");
 	},
 
@@ -40,7 +50,7 @@ JeffreySoundboard.Preloader.prototype = {
 	},
 
 	update: function () {
-        if(this.cache.isSoundDecoded('jeffrey_7_sfx') && this.ready == false) {
+        if(this.cache.isSoundDecoded('jeffrey_12_sfx') && this.ready == false) {
             this.ready = true;
             this.state.start('Game');
         }
