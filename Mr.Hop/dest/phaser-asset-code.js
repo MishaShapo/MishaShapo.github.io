cc3e11c2187d3b57acdd793d9185e5e6d1f32268
Michail Shaposhnikov <misha.shapo98@gmail.com>
2015-11-25 12:05:52 -0600

    Added MrHop

---
+++ Mr.Hop/dest/phaser-asset-code.js
@@ -0,0 +1,9 @@
+this.load.image.('background',['assets/images/background.png']);
+this.load.image.('coin',['assets/images/coin.png']);
+this.load.image.('floor',['assets/images/floor.png']);
+this.load.image.('player_dead',['assets/images/player_dead.png']);
+this.load.spritesheet.('player',['assets/images/player_spritesheet.png']);
+this.load.image.('preloader-bar',['assets/images/preloader-bar.png']);
+this.load.image.('water',['assets/images/water.png']);
+this.load.image.('yellow_block',['assets/images/yellow_block.png']);
+this.load.audio.('coinSFX',['assets/audio/coinSFX.ogg','assets/audio/coinSFX.mp3']);
